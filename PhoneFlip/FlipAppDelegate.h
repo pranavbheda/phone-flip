f698107b25db98a3f417d4d11588332ef725a0d8
Pranav <Pranav@P-Bheda-Air.local>
2013-03-17 20:21:14 -0700

    Initial Commit

---
+++ PhoneFlip/FlipAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FlipAppDelegate.h
+//  PhoneFlip
+//
+//  Created by Pranav on 3/17/13.
+//  Copyright (c) 2013 goa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlipAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhoneFlip/FlipViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlipViewController.h
+//  PhoneFlip
+//
+//  Created by Pranav on 3/17/13.
+//  Copyright (c) 2013 goa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlipViewController : UIViewController
+
+@end
